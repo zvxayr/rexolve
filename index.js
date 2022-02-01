2c0517705bdd0e038b0ff84ca7a29619f9c28e25
Carlo Fornoles <carlo4noles@gmail.com>
2022-02-01 21:26:44 +0800

    Refactored Code
    Extracted object resolution into its own function
    Changed indentations to spaces

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
+function rexolveObject(object) {
+  // Place object contents on arrays to parse in parrallel
+  let keys = Object.keys(object);
+  let values = Object.values(object).map(rexolve);
+  
+  return Promise.all(values).then(resolved => {
+    let result = {};
+    for (let i in keys) {
+      result[keys[i]] = resolved[i];
+    }
+    return result;
+  });
+}
+
 function rexolve(valuePromise) {
-	return Promise.resolve(valuePromise).then(value => {
-		if (value.constructor == Object) {
-			// Place object contents on arrays to parse in parrallel
-			let keys = [], contents = [];
-			for (let key in value) {
-				keys.push(key);
-				contents.push(value[key]);
-			}
-
-			return Promise.all(contents.map(rexolve)).then(resolved => {
-				let result = {};
-				for (let index in keys) {
-					let key = keys[index],
-						content = resolved[index];
-					result[key] = content;
-				}
-				return result;
-			});
-
-		} else if (value.constructor == Array) {
-			// Parse array
-			return Promise.all(value.map(rexolve));
-		} else {
-			return value;
-		}
-	});
+  return Promise.resolve(valuePromise).then(value => {
+    if (value.constructor == Object) {
+      return rexolveObject(value);
+    } else if (value.constructor == Array) {
+      return Promise.all(value.map(rexolve));
+    } else {
+      return value;
+    }
+  });
 }
 
 module.exports = rexolve;
